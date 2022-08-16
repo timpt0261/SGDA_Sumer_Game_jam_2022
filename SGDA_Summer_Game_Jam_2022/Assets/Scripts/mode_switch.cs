9cf96b57946acb9f858c6a2e26682340836eb7c7
timpt0261 <rchavez0215@gmail.com>
2022-08-16 15:58:19 -0700

    First version of switch mode

--- SGDA_Summer_Game_Jam_2022/Assets/Scripts/mode_switch.cs
+++ SGDA_Summer_Game_Jam_2022/Assets/Scripts/mode_switch.cs
@@ -2,38 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main_c : MonoBehaviour
+public class mode_switch : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float speed = 300;
-    private bool mode;
+    private bool Is_human = true;
+
+    float start_time = 0, pressed_time = 0;
+    float cool_time = 3.0f;
+    bool check = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        mode = true;
-
     }
 
-    // Update is called once per frame
+    // Got help from forum
+    // https://answers.unity.com/questions/815394/how-to-get-time-of-key-held-down.html
+
     void Update()
     {
+        
         // play animation to switch to ghost
 
         // is ghost skin
 
-        // movment limiatations
+        // if player is ghost and the left buttton is pressedhold for 1.5 sec
+        if (Input.GetMouseButtonDown(0) && !check)
+        {
+            start_time = Time.time;
+            pressed_time = start_time + cool_time;
+            Debug.Log("Start Time: " + start_time + " ,  Pressed Time " + pressed_time);
+            check = true;
+        }
 
-        Movement();
+        if (Input.GetMouseButtonUp(0))
+        {
+            check = false;
+        }
+
+        if (Time.time >= pressed_time && check)
+        {
+            check = false;
+
+            Movement_Mode();
+            //Debug.Log("Left Mouse button pressed for more than 3 secs");
+
+        }
+
+
+
     }
 
-    void Movement() {
-        if (Input.GetMouseButtonDown(1)) {
-            Debug.Log("In Human Mode");
-        }else if (Input.GetMouseButtonDown(2))
+
+
+    // Determines if player in human mode or ghost mode
+    void Movement_Mode()
+    {
+
+        if (!Is_human)
         {
-            Debug.Log("In Ghost Mode");
+            // play animation from human to ghost
+            // switch to human skin
+
+            // only able two direction 
+            // not able to move through physical enemies
+            // has gravity mass of one
+            Debug.Log("In Human mode");
+
+            Is_human = true;
+            return;
         }
+
+        Debug.Log("In Ghost Mode");
+        Is_human = false;
+        return;
+
     }
 }

8820dba63ff92fb9b11ca8fe3e8810c0dbeeda96
timpt0261 <rchavez0215@gmail.com>
2022-08-15 19:09:06 -0700

    Added Unity Project
    Player is a white square that moves

---
+++ SGDA_Summer_Game_Jam_2022/Assets/Scripts/main_character_movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_character_movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 dir = new Vector2(horizontal, vertical);
+
+        rb.velocity = dir * speed * Time.fixedDeltaTime;
+
+
+    }
+}

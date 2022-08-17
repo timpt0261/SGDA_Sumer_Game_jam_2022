c3070af7929ac10010d3b5643882afc6baf3271d
Ryakuu <ryanfolmar246@gmail.com>
2022-08-17 14:46:09 -0500

    Update basic_enemy_ai.cs
    Some basic stuff. I need to go into unity to confirm some functions and syntax

--- SGDA_Summer_Game_Jam_2022/Assets/Scripts/basic_enemy_ai.cs
+++ SGDA_Summer_Game_Jam_2022/Assets/Scripts/basic_enemy_ai.cs
@@ -12,14 +12,53 @@ public class basic_enemy_ai : MonoBehaviour
 
     // This script will not deal with enemy spawns.
 
+    // Enemies will walk left to right. If they are on a platform, make them switch directions before they fall off.
+    // Have variable patrol paths that can be set up in the editor.
+    // Create a bool that determines if enemy is ranged or not. If we end up having more enemy types, I will come up with a different way
+    // To know which enemy is which.
+
+
+    int HP = 3;
+    public bool isDead = false;
+    SpriteRenderer mySprite;
+    BoxCollider2D myCollider;
+    Rigidbody2D myRigidBody;
+
+
+
     void Start()
     {
-
+        mySprite = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<BoxCollider2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isDead) return;
+    }
 
+    public void DamageEnemy()
+    {
+        if(isDead) return;
+        HP--;
+        if(HP <= 0) // Just being safe :P
+        {
+            StartCoroutine(DeathSequence());
+        }
+    }
+
+    IEnumerator DeathSequence()
+    {
+        isDead = true;
+        // Fling enemy up.
+        // After a few seconds, start fading them out.
+        // Once Opacity is equal to zero, destroy the object.
+
+
+        Destroy(this.gameobject);
+
+        yield return null;
     }
 }

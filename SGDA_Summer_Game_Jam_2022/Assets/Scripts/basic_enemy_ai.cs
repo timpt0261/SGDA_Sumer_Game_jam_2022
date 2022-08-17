8a7bdb30c94b2f5de4fa982081dff1fb6f5e6427
Ryakuu <ryanfolmar246@gmail.com>
2022-08-17 14:21:16 -0500

    Enemy Script
    Just the plans and comments

---
+++ SGDA_Summer_Game_Jam_2022/Assets/Scripts/basic_enemy_ai.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic_enemy_ai : MonoBehaviour
+{
+    // This script will be placed on every instance of every simple enemy.
+    // All Enemies should hurt the player on collision.
+    // If the player hits an enemy, lower hitpoints of enemy.
+    // When an enemy dies, they should play death animation/sprite, and then become untouchable.
+    // Then Fade them out slowly, before destorying the object.
+
+    // This script will not deal with enemy spawns.
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
